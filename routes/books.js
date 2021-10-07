f9dc1ed04eaf2115686426287f515055450737a1
MBurtonshaw <56322461+MBurtonshaw@users.noreply.github.com>
2021-10-07 11:19:16 -0400

    Update & Delete are working; Routing fixed

--- routes/books.js
+++ routes/books.js
@@ -16,13 +16,9 @@ function asyncHandler( cb ){
 
 /* GET users listing. */
 router.get( '/', asyncHandler( async( req, res, next ) => {
-  let books = await Book.findAll();
+  let books;
+  books = await Book.findAll();
   res.render( 'index', { books, title: 'Library Database' });
-}));
-
-router.post( '/', asyncHandler( async( req, res, next ) => {
-  let book = await Book.create(req.body);
-  res.redirect( '/books/' + book.id );
 }));
 
 router.get( '/page-not-found', asyncHandler( async( req, res, next ) => {
@@ -33,26 +29,56 @@ router.get( '/new', asyncHandler( async( req, res, next ) => {
   res.render( 'new-book', {book: {}, title: 'New Book'});
 }));
 
+router.post( '/new', asyncHandler( async( req, res, next ) => {
+  let book;
+  try {
+  book = await Book.create(req.body);
+  console.log(book.title + ' by ' + book.author + ' has been added to the database');
+  res.redirect( '/books' );
+  } catch(err) {
+    if(err.name === 'SequelizeValidationError') {
+      book = await Book.build(req.body);
+      res.render('/books/new', { book, errors: error.errors, title: 'New Article' });
+    } else {
+      throw error;
+    }
+  }
+}));
+
 router.get( '/:id', asyncHandler( async( req, res, next ) => {
-  const book = await Book.findByPk(req.params.id);
-  res.render( 'update-book', { book, title: 'Update Book' } );
+  let book;
+  book = await Book.findByPk(req.params.id);
+  if (book) {
+      res.render( 'update-book', { book, title: book.title } );
+  } else {
+    res.status(404).render('page-not-found');
+  }
 }));
 
 router.post( '/:id', asyncHandler( async( req, res, next )=>{
-  let book = await Book.findByPk(req.params.id);
-  await book.update(req.body);
-  res.redirect('/books');
+  let book;
+    book = await Book.findByPk(req.params.id);
+    await book.update(req.body);
+    console.log(book.title + ' by ' + book.author + ' has been updated');
+    res.redirect('/books');
 }));
 
-router.get( '/:id/delete', asyncHandler( async( req, res, next )=>{
-  let book = await Book.findByPk(req.params.id);
-  res.render( 'delete-book', { book, title: 'Delete book' });
+router.get( '/:id/delete', asyncHandler(async(req, res) => {
+  let book;
+  book = await Book.findByPk(req.params.id);
+  if (book) {
+    res.render( 'delete-book', { book, title: 'Delete Book' });
+  } else {
+    res.status(404).render('page-not-found');
+  }
 }));
 
 router.post( '/:id/delete', asyncHandler( async( req, res, next )=>{
-  let book = await Book.findByPk(req.params.id);
-  await book.destroy();
-  res.redirect('/books');
+  let book;
+  book = await Book.findByPk(req.params.id);
+    await book.destroy();
+    console.log(book.title + ' by ' + book.author + ' has been deleted');
+    res.redirect('/books');
 }));
 
 module.exports = router;
